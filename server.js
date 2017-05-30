900b5f3ad4c8bf8c5553e757fd6d7c19092ffded
Benoît Hubert <benoithubert@gmail.com>
2017-05-30 09:03:17 +0200

    Don't create account if exists, don't create token if exists

--- server.js
+++ server.js
@@ -12,11 +12,32 @@ const { router, middlewares, queryBuilder, queryAsync } = require('jsonapi-expre
 const port = config.port || 3001;
 
 const objWrapper = {
-  createOne: function(table, attributes) {
+  create: function(table, attributes) {
     const insertQuery = queryBuilder.insert(table, attributes);
     return queryAsync(insertQuery)
     .then(res => queryBuilder.selectOne(table, res.insertId))
     .then(queryAsync)
+    .then(records => Object.assign({}, records[0]));
+  },
+
+  findById: function(table, id) {
+    const selectQuery = queryBuilder.selectOne(table, id);
+    return queryAsync(selectQuery)
+    .then(records => Object.assign({}, records[0]));
+  },
+
+  findBy: function(table, where, multi) {
+    const selectQuery = queryBuilder.selectWhere(table, where);
+    return queryAsync(selectQuery)
+    .then(records => (!! multi ? records :
+      (records.length ? records[0] : false)));
+  },
+
+  update: function(table, id, attributes) {
+    const updateQuery = queryBuilder.updateOne(table, id, attributes);
+    const selectQuery = queryBuilder.selectOne(table, id);
+    return queryAsync(updateQuery)
+    .then(() => queryAsync(selectQuery))
     .then(records => Object.assign({}, records[0]));
   }
 }
@@ -60,13 +81,32 @@ app.get('/api/v1/client-ids', (req, res) => {
   res.jsonApi(clientIds);
 });
 
+function getAccount(userId, provider, username) {
+  return objWrapper.findBy('accounts', { userId, username, type: provider });
+}
+
+function getToken(accountId) {
+  return objWrapper.findBy('api_tokens', { accountId });
+}
+
 app.post('/api/v1/got/:provider', (req, res) => {
+
+  // Get params
+  //   * From request: provider (bitbucket, github, etc.)
+  //   * From JWT: current user's ID
+  //   * From config: Client ID (key) and secret for provider
   const { provider } = req.params;
   const { userId } = req.jwt;
   const params = config.clientIds[provider];
+
+  // Encode provider credentials to base64
   const rawCredentials = params.clientId + ':' + params.secret;
   const encodedCredentials = new Buffer(rawCredentials).toString('base64');
-  console.log(params, rawCredentials, encodedCredentials);
+
+  let mustCreateAccount;
+  let mustCreateToken;
+
+  // Prepare request to provider's access token route
   const options = {
     method: 'POST',
     uri: 'https://bitbucket.org/site/oauth2/access_token',
@@ -76,25 +116,50 @@ app.post('/api/v1/got/:provider', (req, res) => {
     },
     headers: {
       Authorization: 'Basic ' + encodedCredentials
-    },
-    // json: true
+    }
   };
-  console.log(options);
+
+  // Fire request, store parsed JSON response
   chain(request(options))
   .then(response => JSON.parse(response))
   .set('token')
-  .then(responseBody => {
-    console.log('## Access token', responseBody.access_token);
-    return repoApis.bitbucket.setToken(responseBody.access_token);
-  })
+  // Setup the provider strategy with freshly got access token
+  .then(token => repoApis.bitbucket.setToken(token.access_token))
   .set('apiUser')
-  .then(user => {
-    const { username } = user;
-    const name = username + '@' + provider;
-    const accountAttrs = {
-      type: provider, username, userId, name 
-    };
-    return objWrapper.createOne('accounts', accountAttrs)
+  .then(({ username }) => {
+    return getAccount(userId, provider, username)
+    .then(account => {
+      // Bypass if an account already exists
+      mustCreateAccount = account === false;
+      if(! mustCreateAccount) {
+        return account;
+      }
+      // Otherwise create
+      const name = username + '@' + provider;
+      const accountAttrs = {
+        type: provider, username, userId, name 
+      };
+      return objWrapper.create('accounts', accountAttrs)
+    });
+  })
+  .set('account')
+  .then(({ token, apiUser, account }) => {
+    return getToken(account.id)
+    .then(tokenRecord => {
+      mustCreateToken = tokenRecord === false;
+      let tokenAttrs = {
+        access_token: token.access_token,
+        refresh_token: token.refresh_token
+      };
+      // If a token already exists for this account, update it
+      if(! mustCreateToken) {
+        return objWrapper.update('api_tokens', tokenRecord.id, tokenAttrs);
+      }
+      tokenAttrs.accountId = account.id;
+      return objWrapper.create('api_tokens', tokenAttrs);
+    });
+  })
+
     // console.log('## User', user);
     // repoApis.bitbucket.getProjects()
     // .then(projectsRes => {
@@ -105,19 +170,18 @@ app.post('/api/v1/got/:provider', (req, res) => {
     //   });
     // })
 
-  })
-  .then(attrs => { console.log('account attrs', attrs); return attrs; })
-  .set('account')
-  .get(({ account, token }) => ({
-    accountId: account.id,
-    access_token: token.access_token,
-    refresh_token: token.refresh_token
-  }))
-  .then(attrs => { console.log('token attrs', attrs); return attrs; })
-  .then(tokenAttrs => objWrapper.createOne('api_tokens', tokenAttrs))
+  // .get(({ account, token }) => ({
+  //   accountId: account.id,
+  //   access_token: token.access_token,
+  //   refresh_token: token.refresh_token
+  // }))
+  // .then(attrs => { console.log('token attrs', attrs); return attrs; })
+  // .then(tokenAttrs => objWrapper.create('api_tokens', tokenAttrs))
   .set('tokenRecord')
   .get(data => {
-    res.json(data);    
+    res.json(Object.assign(data, {
+      mustCreateAccount, mustCreateToken
+    }));    
   })
   .catch(err => {
     console.log(err);
