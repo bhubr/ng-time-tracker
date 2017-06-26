0649faf094b372ca872f4f72e31a6e39ae2c0605
Benoît Hubert <benoithubert@gmail.com>
2017-06-26 16:32:26 +0200

    save selected project&issue in local storage

--- ng-app/components/TimerSetupComponent.js
+++ ng-app/components/TimerSetupComponent.js
@@ -3,10 +3,12 @@ TimerSetupController.$inject = ['lodash', 'dataService'];
 function TimerSetupController(_, dataService) {
   console.log('TimerSetupController init', this);
   const self = this;
+  const storedProjectId = localStorage.getItem('storedProjectId');
+  const storedIssueId = localStorage.getItem('storedIssueId');
 
   this.filters = {
-    projectId: '',
-    issueId: ''
+    projectId: storedProjectId !== null ? storedProjectId : 0,
+    issueId: storedIssueId !== null ? storedIssueId : 0
   };
   this.issueOptions = [];
   
@@ -17,6 +19,8 @@ function TimerSetupController(_, dataService) {
       const project = _.find(this.projectOptions,{ id })
       this.syncProjectIssues(project);
     }
+    localStorage.setItem('storedProjectId', id);
+    localStorage.removeItem('storedIssueId');
   }
 
 
@@ -24,13 +28,15 @@ function TimerSetupController(_, dataService) {
     console.log('TimerSetupController.syncProjectIssues', project);
     dataService.syncProjectIssues(project)
     .then(issues => {
-      self.issueOptions = [{ id: 0, name: '' }].concat(issues);
+      self.issueOptions = [{ id: 0, title: '' }].concat(issues);
       console.log('TimerSetupController $scope.issueOptions', self.issueOptions);
     })
   }
 
   this.selectIssue = function() {
     console.log('TimerSetupController.selectIssue', this.filters);
+    const id = this.filters.issueId;
+    localStorage.setItem('storedIssueId', id);
   }
 }
 
